327a35587e25f8d992f8a7b03d35e2a29d3f825c
Jason Ibrahim <jasonjwilliam@hotmail.com>
2026-08-25 19:51:52 +0200

    Add initial load function with test data

---
+++ load_steam.py
@@ -0,0 +1,18 @@
+def load_data(data):
+    print(data) 
+
+
+test_data = [
+    {
+        "appid": 450,
+        "name": "GTA 6",
+        "playtime_hours": 15.5
+    },
+    {
+        "appid": 422,
+        "name": "call of duty",
+        "playtime_hours": 14.6
+    }
+]
+
+load_data(test_data)
