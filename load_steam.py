3f5ffdce65c80e9c5aca2249e549d31fe003d696
Jason Ibrahim <jasonjwilliam@hotmail.com>
2026-09-01 12:09:23 +0200

    Add CSV input reader

---
+++ load_steam.py
@@ -0,0 +1,22 @@
+import psycopg2
+import csv
+
+
+def read_csv_data(input_file):
+    with open(input_file, "r", encoding="utf-8") as file:
+        reader = csv.DictReader(file)
+        return list(reader)
+
+
+def get_db_connetion():
+
+
+
+def create_games_table(connection):
+
+
+def insert_games(connection, rows): 
+
+
+def load_data(input_file):
+
